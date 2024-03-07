3f884a96f7b41110db586a49b5c8db85f8c2be5a
sehrish00452840 <sehrish.siddiq98@gmail.com>
2024-03-07 19:18:47 +0500

    first commit

--- task 2/main.js
+++ task 2/main.js
@@ -1,2 +1,2 @@
 var personName = "sehar";
-console.log("Hello", personName, ",would u like to learn some python today?");
+console.log("Hello", personName, "would u like to learn some python today?");
